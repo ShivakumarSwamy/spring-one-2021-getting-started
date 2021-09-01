2df3e8ec55da12493005f8f0d198b910ebd4074d
Shivakumar Swamy <shiva.swamy@logmein.com>
2021-09-01 17:02:54 +0200

    Initial commit

---
+++ src/main/java/com/example/springone2021gettingstarted/SpringOne2021GettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springone2021gettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOne2021GettingStartedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOne2021GettingStartedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springone2021gettingstarted/SpringOne2021GettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springone2021gettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOne2021GettingStartedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
